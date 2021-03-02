05d2ee55f1a9ee151ac46dbaabc9e139dd0f6566
normen-zzz <normanardian24@gmail.com>
2021-03-02 21:28:15 +0700

    template

---
+++ application/views/admin/index.php
@@ -0,0 +1,19 @@
+<?php $this->load->view('admin/template/header') ?>
+<?php $this->load->view('admin/template/topbar') ?>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper">
+      
+     <?php $this->load->view('admin/template/sidebar') ?>
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          
+        </section>
+      </div>
+      
+  </div>
+
+  <?php $this->load->view('admin/template/footer') ?>
