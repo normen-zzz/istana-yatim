77e5360256f5c722e9c6a4416e75fef203d681e3
normen-zzz <normanardian24@gmail.com>
2021-04-11 09:51:08 +0700

    Update login admin

---
+++ application/controllers/Admin.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->session->set_flashdata('not-login', 'Gagal!');
+        if (!$this->session->userdata('email')) {
+            redirect('Auth/Admin');
+        }
+    }
+
+
+	public function index()
+	{
+		$data['user'] = $this->db->get_where('pengurus', ['email_pengurus' =>$this->session->userdata('email')])->row_array();
+
+		$this->load->view('admin/index',$data);
+	}
+}
---
+++ application/controllers/Auth.php
@@ -0,0 +1,65 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth extends CI_Controller
+{
+
+
+    public function admin()
+    {
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', [
+            'required' => 'Harap isi bidang email!',
+            'valid_email' => 'Email tidak valid!',
+        ]);
+        $this->form_validation->set_rules('password', 'Password', 'trim|required', [
+            'required' => 'Harap isi bidang password!',
+        ]);
+        if ($this->form_validation->run() == false) {
+            $this->load->view('admin/loginn');
+        } else {
+            //validasi sukses
+            $this->adminlogin();
+        }
+    }
+
+    private function adminlogin()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('pengurus', ['email_pengurus' => $email])->row_array();
+
+        if ($user) {
+            //cek password
+            if (password_verify($password, $user['password_pengurus'])) {
+                $data = [
+
+                    'email' => $user['email_pengurus'],
+                    'nama' => $user['nm_pengurus'],
+
+                ];
+                $this->session->set_userdata($data);
+                redirect(base_url('admin'));
+            } else {
+
+                $this->session->set_flashdata('fail-pass', 'Gagal!');
+                redirect(base_url('auth/admin'));
+            }
+        } else {
+
+            $this->session->set_flashdata('fail-login', 'Gagal!');
+            redirect(base_url('auth/admin'));
+        }
+    }
+
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->set_flashdata('success-logout', 'Berhasil!');
+        redirect(base_url('Auth/admin'));
+    }
+
+
+
+}
