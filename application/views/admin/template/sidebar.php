1e7bffc3370527761e61d88e070ed9e7f7d08d0d
normen-zzz <normanardian24@gmail.com>
2021-04-11 10:03:51 +0700

    .

--- application/views/admin/template/sidebar.php
+++ application/views/admin/template/sidebar.php
@@ -7,7 +7,7 @@
             <a href="index.html">St</a>
           </div>
           <ul class="sidebar-menu">
-              <li class="menu-header">Dashboard</li>
+              <li class="menu-header">CMS</li>
               <li class="nav-item dropdown active">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
                 <ul class="dropdown-menu">
---
+++ database/sipena.sql
@@ -0,0 +1,191 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 11, 2021 at 05:03 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sipena`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `artikel`
+--
+
+CREATE TABLE `artikel` (
+  `kd_artikel` int(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `judul_artikel` varchar(123) NOT NULL,
+  `isi_artikel` text NOT NULL,
+  `penulis_artikel` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donasi`
+--
+
+CREATE TABLE `donasi` (
+  `no.invoice` int(20) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah` int(13) NOT NULL,
+  `bukti` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `donatur`
+--
+
+CREATE TABLE `donatur` (
+  `id_donatur` int(8) NOT NULL,
+  `nama_donatur` varchar(20) NOT NULL,
+  `email_donatur` text NOT NULL,
+  `password_donatur` text NOT NULL,
+  `role` int(11) NOT NULL DEFAULT '2'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `metode`
+--
+
+CREATE TABLE `metode` (
+  `id_metode` int(11) NOT NULL,
+  `bri` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengurus`
+--
+
+CREATE TABLE `pengurus` (
+  `id_pengurus` int(8) NOT NULL,
+  `nm_pengurus` varchar(20) NOT NULL,
+  `umur_pengurus` varchar(3) NOT NULL,
+  `alamat_pengurus` text NOT NULL,
+  `no.telp` varchar(13) NOT NULL,
+  `foto_pengurus` text NOT NULL,
+  `email_pengurus` text NOT NULL,
+  `password_pengurus` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengurus`
+--
+
+INSERT INTO `pengurus` (`id_pengurus`, `nm_pengurus`, `umur_pengurus`, `alamat_pengurus`, `no.telp`, `foto_pengurus`, `email_pengurus`, `password_pengurus`) VALUES
+(1, 'admin', '20', 'nuuunscjhs', '0987668', 'default.img\r\n', 'admin@gmail.com', '$2y$10$EX0L5MeIQldpkCuTZW.mjujTaj.Yy20IW0GOluecU/c.es.9r6E5.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `santri`
+--
+
+CREATE TABLE `santri` (
+  `id_santri` int(8) NOT NULL,
+  `nama_santri` varchar(20) NOT NULL,
+  `umur_santri` varchar(3) NOT NULL,
+  `no.telp` varchar(13) NOT NULL,
+  `ttl_santri` date NOT NULL,
+  `foto_santri` text NOT NULL,
+  `jns_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `email_santri` text NOT NULL,
+  `password_santri` text NOT NULL,
+  `role` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `artikel`
+--
+ALTER TABLE `artikel`
+  ADD PRIMARY KEY (`kd_artikel`);
+
+--
+-- Indexes for table `donasi`
+--
+ALTER TABLE `donasi`
+  ADD PRIMARY KEY (`no.invoice`);
+
+--
+-- Indexes for table `donatur`
+--
+ALTER TABLE `donatur`
+  ADD PRIMARY KEY (`id_donatur`);
+
+--
+-- Indexes for table `metode`
+--
+ALTER TABLE `metode`
+  ADD PRIMARY KEY (`id_metode`);
+
+--
+-- Indexes for table `pengurus`
+--
+ALTER TABLE `pengurus`
+  ADD PRIMARY KEY (`id_pengurus`);
+
+--
+-- Indexes for table `santri`
+--
+ALTER TABLE `santri`
+  ADD PRIMARY KEY (`id_santri`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `artikel`
+--
+ALTER TABLE `artikel`
+  MODIFY `kd_artikel` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `donatur`
+--
+ALTER TABLE `donatur`
+  MODIFY `id_donatur` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengurus`
+--
+ALTER TABLE `pengurus`
+  MODIFY `id_pengurus` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `santri`
+--
+ALTER TABLE `santri`
+  MODIFY `id_santri` int(8) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
