344d43bc0724e2dec502440a3632c79966eb4406
AlifNopian <alifnopian94@gmail.com>
2021-04-17 10:06:16 +0700

    ubah form login admin

--- application/views/admin/loginn.php
+++ application/views/admin/loginn.php
@@ -27,7 +27,7 @@
             </div>
 
             <div class="card card-primary">
-              <div class="card-header"><h4>Login</h4></div>
+              <div class="card-header"><h4>Masuk Admin</h4></div>
 
               <div class="card-body">
                 <form method="POST" action="#" class="needs-validation" novalidate="">
@@ -54,12 +54,12 @@
                     </div>
                   </div>
 
-                  <div class="form-group">
+<!--                   <div class="form-group">
                     <div class="custom-control custom-checkbox">
                       <input type="checkbox" name="remember" class="custom-control-input" tabindex="3" id="remember-me">
                       <label class="custom-control-label" for="remember-me">Remember Me</label>
                     </div>
-                  </div>
+                  </div> -->
 
                   <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
@@ -67,7 +67,7 @@
                     </button>
                   </div>
                 </form>
-                <div class="text-center mt-4 mb-3">
+<!--                 <div class="text-center mt-4 mb-3">
                   <div class="text-job text-muted">Login With Social</div>
                 </div>
                 <div class="row sm-gutters">
@@ -81,16 +81,16 @@
                       <span class="fab fa-twitter"></span> Twitter
                     </a>
                   </div>
-                </div>
+                </div> -->
 
               </div>
             </div>
-            <div class="mt-5 text-muted text-center">
+<!--             <div class="mt-5 text-muted text-center">
               Don't have an account? <a href="auth-register.html">Create One</a>
             </div>
             <div class="simple-footer">
               Copyright &copy; Stisla 2018
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
