be1d40278e4d6cd9611979b9639962a60a7ab0e4
normen-zzz <normanardian24@gmail.com>
2021-04-17 11:55:02 +0700

    .

--- application/views/User/index.php
+++ application/views/User/index.php
@@ -114,7 +114,7 @@
                         <li><a href="#">Benefits</a></li>
                     </ul>
                 </div>
-                <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
                     <p class="copyright">Company Name © 2017</p>
                 </div>
             </div>
