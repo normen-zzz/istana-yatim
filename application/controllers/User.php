d37670dcd6da2ed5d58ca932ff126b808af6727f
AlifNopian <alifnopian94@gmail.com>
2021-04-17 09:00:18 +0700

    Landing Page

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,16 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller {
 
-	public function __construct()
-    {
+	public function __construct(){
         parent::__construct();
         $this->load->helper('url');
         $this->session->set_flashdata('not-login', 'Gagal!');
-        }
+    }
 
-
-	public function index()
-	{
+	public function index(){
 		$this->load->model('M_menu');
 		$this->load->model('M_slidefoto');
 		$data['active'] = 'active';
