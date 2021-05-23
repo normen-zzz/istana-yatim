88e865416359d171d325c38543a977a4b5edd1c7
normen-zzz <normanardian24@gmail.com>
2021-05-23 16:30:04 +0700

    pdf

--- application/views/admin/donasi/sudahkonfirmasi.php
+++ application/views/admin/donasi/sudahkonfirmasi.php
@@ -1,4 +1,5 @@
 <?php $this->load->view('admin/template/header') ?>
+<link rel="stylesheet" type="text/css" href="">
 
 <?php $this->load->view('admin/template/topbar') ?>
 
@@ -157,14 +158,29 @@
 <?php endif;?>
 
 <?php $this->load->view('admin/template/footer') ?>
-<!-- <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"></script> -->
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/dataTables.buttons.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.print.min.js"></script>
+
 
 <script type="text/javascript">
-  $(document).ready( function () {
-    $('#myTable').DataTable();
-  } );
+  $(document).ready(function() {
+    $('#myTable').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    } );
+} );
 </script>
+
+
 
 <script type="text/javascript">
   function confir(){
