f5e212edc554e56bf709be5386403e8b4d5b638c
normanardian <71215112+normen-zzz@users.noreply.github.com>
2021-12-08 11:07:02 +0700

    Fixing

--- application/views/event.php
+++ application/views/event.php
@@ -24,7 +24,7 @@
                           </div>
                       </div>
                   </div>
-              <? } ?>
+              <?php } ?>
           </div>
       </div>
   </section>
